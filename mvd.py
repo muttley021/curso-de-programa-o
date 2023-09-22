4ad4c4b3bcdfd28ed3690c1fba9d64448767f8fa
muttley021 <125143570+muttley021@users.noreply.github.com>
2023-09-22 17:26:09 -0300

    total da compra
    faz sua compra aqui

---
+++ mvd.py
@@ -0,0 +1,13 @@
+#produdo quantidade valor emprimer tudo depois quandida x valor 
+nos=["feijao", "arroz", "carne","molho", "alho"]
+vas=[10, 5,20, 3, 40]
+qnts=[3, 9,8,30, 21]
+for i in range(len(nos)):
+    no = nos[i]
+    nu = vas[i]
+    qnt = qnts[i]
+    print(f"no:{no}, nu:{nu} qnt;{qnt}")
+    print("valor total:",qnt*nu)
+    
+
+ 
