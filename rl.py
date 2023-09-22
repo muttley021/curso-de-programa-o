447a83172b5ba5097fe59773559a1c0d905e3ea3
muttley021 <125143570+muttley021@users.noreply.github.com>
2023-09-22 16:45:17 -0300

    nome e numero
    lista de numero e nome

---
+++ rl.py
@@ -0,0 +1,7 @@
+nos=["luis","eduardo","erika","jesus"]
+nus=["(99)6400-8492","(45)7699-0321","(57)6500-4533","(87)8424-0075"]
+#usa o loop para imprimir nome e numero da msm posiçao
+for i in range(len(nos)):
+    no = nos[i]
+    nu = nus[i]
+    print(f"no:{no}, nu:{nu}")
