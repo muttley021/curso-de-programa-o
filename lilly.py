2e46c8014e159efd6615dce865930ef02c30842c
muttley021 <125143570+muttley021@users.noreply.github.com>
2023-09-26 16:19:53 -0300

    tranformando minutos wm horas

---
+++ lilly.py
@@ -0,0 +1,5 @@
+tamanho = float(input('Tamanho do arquivo (MB): '))
+velocidade = float(input('Velocidade de Internet (MBps): '))
+print('Tempo aproximado de download: %.0f Minutos ' %((tamanho / velocidade) * 60) )
+soma=(tamanho*8) / (velocidade*60)
+print(soma)
